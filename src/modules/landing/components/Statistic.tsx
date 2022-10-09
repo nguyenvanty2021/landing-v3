b3597c78d6ef2b125fc427960825a7a1a23e47bd
Tỷ Nguyễn <tynguyen@Tys-MacBook-Pro.local>
2022-10-09 12:34:44 +0700

    revert img

--- src/modules/landing/components/Statistic.tsx
+++ src/modules/landing/components/Statistic.tsx
@@ -1,4 +1,5 @@
-
+import Image from 'next/image';
+import LazyLoadCommon from 'common/components/lazyLoad';
 import React, { useMemo } from 'react';
 export default function Statistic() {
 	const statisticBoxes = useMemo(() => {
@@ -33,7 +34,15 @@ export default function Statistic() {
 	return (
 		<section className='desktop:px-[unset] flex flex-col items-center w-full mt-[3.4375rem] desktop:mt-[1rem] px-[2rem]'>
 			<div className={`w-[1.9169rem] h-[4.375rem] object-contain`}>
-
+				<LazyLoadCommon>
+					<Image
+						width={30.67}
+						height={70}
+						src='/images/roadmap_0.svg'
+						alt='roadmap'
+						objectFit='contain'
+					/>
+				</LazyLoadCommon>
 			</div>
 			<div className='desktop:flex grid tablet:grid-cols-2 flex-col tablet:w-[unset] gap-[30px] desktop:gap-[unset] justify-center items-center desktop:w-full mt-[2.375rem] desktop:flex-row'>
 				{statisticBoxes.map((box, index) => (
@@ -46,10 +55,12 @@ export default function Statistic() {
 
 function StatisticBox({
 	label,
-
+	icon,
+	width,
 }: {
 	label: string;
-
+	icon: string;
+	width: number;
 }) {
 	return (
 		<div
@@ -59,7 +70,9 @@ function StatisticBox({
 			className='desktop:mr-[1.875rem] last:mb-[0] desktop:last:mb-[unset] last:mr-[0] aspect-[1] desktop:aspect-[unset] grow-[0] order-[0] flex-none backgroundStatistic py-[3.125rem] px-[1.9375rem] flex flex-col justify-center items-center h-[19.4375rem] desktop:h-[15.625rem] w-[19.4375rem] desktop:w-[16.4375rem] desktop:mb-[unset] '
 		>
 			<div className='w-[4.5rem] flex justify-center h-[4.5rem]'>
-
+				<LazyLoadCommon>
+					<Image width={width} objectFit='contain' className='flex-[none] order-[0] grow-[0]' height={60} src={icon} alt={icon} />
+				</LazyLoadCommon>
 			</div>
 			<p className='flex text-center text-white flex-none order-[0] grow-[0] mt-[1.25rem] leading-[42px] text-[2rem] font-[600]'>123</p>
 			<p className='flex text-center flex-none order-[1] grow-[0] opacity-[0.6] text-white leading-[24px] text-[1rem]'>{label}</p>
