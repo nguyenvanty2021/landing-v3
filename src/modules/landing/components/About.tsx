90c9fea181c1e6515eef07c333901f7cbfc2da12
Tỷ Nguyễn <tynguyen@Tys-MacBook-Pro.local>
2022-10-09 14:58:40 +0700

    revert code

--- src/modules/landing/components/About.tsx
+++ src/modules/landing/components/About.tsx
@@ -52,14 +52,14 @@ export default function About() {
 							<picture>
 								<source
 									media={`(min-width: ${BREAK_POINT.M_TABLET}px)`}
-									srcSet='/images/about-section.png'
+									srcSet='/images/about-section.svg'
 								/>
 								<source
 									media={`(max-width: ${BREAK_POINT.SM_TABLET}px)`}
-									srcSet='/images/about-sectionMobile.png'
+									srcSet='/images/about-sectionMobile.svg'
 								/>
 								<img
-									src='/images/about-section.png'
+									src='/images/about-section.svg'
 									alt='logo'
 									className='w-[766px] h-[604.52px] object-contain'
 								/>
